a05f09ef514626f6d940f0f13f09037a39fa4c4c
Chandan-Bhat <chandanbhat6@gmail.com>
2022-09-15 21:30:57 +0530

    2.8th stage of landing page

--- landing_Page/app.js
+++ landing_Page/app.js
@@ -1,4 +1,24 @@
-let dat
-fetch("data.json").then(response => response.json).then((data) => {dat = data})
+import data from './data.json' assert { type: 'json' };
+console.log(data);
 
-console(dat["1"].head)
+let index = 0;
+
+const img = document.getElementsByClassName("prod-img")[0];
+const head = document.getElementsByClassName("prod-head")[0];
+const price = document.getElementsByClassName("btn")[0];
+const desc = document.getElementsByClassName("prod-desc")[0];
+
+const frwd = document.getElementById("frwd");
+const bkwd = document.getElementById("bkwd");
+
+frwd.addEventListener("click", forward)
+
+
+
+function forward(){
+    index = (index + 1) % 5;
+    img.setAttribute("src", data[index].img);
+    head.innerText = data[index].head;
+    price.innerText = `BUY NOW at $${data[index].price}`;
+    desc.innerText = data[index].desc;
+}
