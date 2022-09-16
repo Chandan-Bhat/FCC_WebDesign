455554e0ac7675beb30b4b0d7b8f50de3220ad93
Chandan-Bhat <chandanbhat6@gmail.com>
2022-09-16 10:28:44 +0530

    3rd stage of landing page

--- landing_Page/app.js
+++ landing_Page/app.js
@@ -3,7 +3,7 @@ console.log(data);
 
 let index = 0;
 
-const img = document.getElementsByClassName("prod-img")[0];
+const img = document.getElementsByClassName("imgs");
 const head = document.getElementsByClassName("prod-head")[0];
 const price = document.getElementsByClassName("btn")[0];
 const desc = document.getElementsByClassName("prod-desc")[0];
@@ -11,13 +11,32 @@ const desc = document.getElementsByClassName("prod-desc")[0];
 const frwd = document.getElementById("frwd");
 const bkwd = document.getElementById("bkwd");
 
-frwd.addEventListener("click", forward)
-
-
+frwd.addEventListener("click", forward);
+bkwd.addEventListener("click", backward);
 
 function forward(){
+    
+    img[index].classList.toggle("active");
+    img[index].classList.toggle("inactive");
+
     index = (index + 1) % 5;
-    img.setAttribute("src", data[index].img);
+
+    img[index].classList.toggle("inactive");
+    img[index].classList.toggle("active");
+    head.innerText = data[index].head;
+    price.innerText = `BUY NOW at $${data[index].price}`;
+    desc.innerText = data[index].desc;
+}
+
+function backward() {
+    img[index].classList.toggle("active");
+    img[index].classList.toggle("inactive");
+
+    index = (index - 1);
+    if(index < 0) index = 0;
+
+    img[index].classList.toggle("inactive");
+    img[index].classList.toggle("active");
     head.innerText = data[index].head;
     price.innerText = `BUY NOW at $${data[index].price}`;
     desc.innerText = data[index].desc;
